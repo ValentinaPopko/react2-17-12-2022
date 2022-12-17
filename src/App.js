e758ead035da818b89c0619448c299ef37342b9b
Valentina <110343367+ValentinaPopko@users.noreply.github.com>
2022-12-17 15:06:19 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div>
+    <p className = "new1">HELLO WORLD</p>
+    <p className = "new2">HELLO WORLD</p>
+    <p className = "new3">HELLO WORLD</p>
+    <p className = "new4">HELLO WORLD</p>
+    <p className = "new5">HELLO WORLD</p>
+  </div>
+  );
+}
+
+export default App;
